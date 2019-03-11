33efb91c5084335ab290bc9927e46d2b07644d45
“jonyshi” <jonyshi@126.com>
2019-03-11 11:24:28 +0800

    publish 2.0.0

--- build/InputGroupAddon.js
+++ build/InputGroupAddon.js
@@ -40,11 +40,10 @@ var InputGroupAddon = function (_React$Component) {
   }
 
   InputGroupAddon.prototype.render = function render() {
-    var _props = this.props;
-    var className = _props.className;
-    var clsPrefix = _props.clsPrefix;
-
-    var others = _objectWithoutProperties(_props, ['className', 'clsPrefix']);
+    var _props = this.props,
+        className = _props.className,
+        clsPrefix = _props.clsPrefix,
+        others = _objectWithoutProperties(_props, ['className', 'clsPrefix']);
 
     return _react2["default"].createElement('span', _extends({}, others, {
       className: (0, _classnames2["default"])(className, clsPrefix)
